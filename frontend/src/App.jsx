1a4843dba17db20b88b9beb5c78a5c0c93bc5059
Private Use <0x92dev@gmail.com>
2025-09-03 16:18:47 +0200

    feat: implement global search UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DocumentBrowser from './components/DocumentBrowser';
+import GlobalSearch from './components/GlobalSearch';
 
 function App() {
   return (
     <div className="App">
       <h1>Document Browser</h1>
+      <GlobalSearch />
       <DocumentBrowser />
     </div>
   );
